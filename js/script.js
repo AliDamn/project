f48ec442dab8730fa71323ade895880fe56da028
Ivan <example@gmail.com>
2024-09-11 19:35:33 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,21 @@
+"use strict"
+//alert('Hello');
+//const result =confirm('Are you cooker?');
+//console.log(result)
+
+//const answer = +prompt("Do you have 18 years old?","18");
+//console.log(answer+5);
+
+//const category = 'toys';
+//console.log("https://someulr.com/"+category)
+
+console.log(4+'-object');
+console.log(4+ +'5');
+
+let anc=10,
+    decr=10;
+
+anc++;
+decr--;
+console.log(anc);
+console.log(decr);
